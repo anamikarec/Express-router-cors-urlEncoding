9effdc387834d6765d99c3c7956fbb5379540469
Anamika <anamikarecsonbhadra@gmail.com>
2022-02-01 23:24:58 +0530

    express.js basic introduction

---
+++ api/users/User.js
@@ -0,0 +1,24 @@
+const Users=[
+    {
+        id: 1,
+        name: "Anamika",
+        active: true
+    },
+    {
+        id: 2,
+        name: "Albert",
+        active: true
+    },
+    {
+        id: 3,
+        name: "Sneha",
+        active: true
+    },
+    {
+        id: 4,
+        name: "Arjun",
+        active: true
+    }
+]
+
+module.exports= Users;
